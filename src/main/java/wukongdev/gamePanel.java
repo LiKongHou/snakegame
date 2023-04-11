dccfa7dc26337edd24a33815b8b5c556fb04a66e
wuk0ng <l3abysexy@gmail.com>
2023-04-11 22:38:18 +0700

    Update snakeGame v0.1.2038110423

--- src/main/java/wukongdev/gamePanel.java
+++ src/main/java/wukongdev/gamePanel.java
@@ -13,7 +13,7 @@ public class gamePanel extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 800;
     static final int SCREEN_HEIGHT = 800;
     static final int UNIT_SIZE = 10;
-    static final int DELAY = 40;
+    static final int DELAY = 50;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / (UNIT_SIZE * UNIT_SIZE);
     final int[] xGame = new int[GAME_UNITS];
     final int[] yGame = new int[GAME_UNITS];
